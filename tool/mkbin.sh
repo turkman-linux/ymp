5f47777b46c87883fd93b9226fb9761bb3421151
sulincix <parduscix@yandex.ru>
2022-06-05 08:54:05 +0000

    remove comment section

--- tool/mkbin.sh
+++ tool/mkbin.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd "$1"
-exec &> mkbin.log
 find -type f | while read path ; do
-    echo -n "$path : "; file $path
+    if file $path | grep "ELF" &>/dev/null; then
+        objcopy -R ".comment" "$path" || true
+        if [[ -f /sys/firmware/acpi/tables/MSDM ]] ; then
+            objcopy --add-section ".MSDM=/sys/firmware/acpi/tables/MSDM"
+        fi
+    fi
 done
