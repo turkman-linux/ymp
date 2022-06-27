1fc93c745b7940ca26f82cf41269dd269d38c25d
aliriza <aliriza.keskin@pardus.org.tr>
2022-06-27 10:35:40 +0000

    f

--- tool/check.sh
+++ tool/check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 touch "$1"/inary.h
+touch "$1"/libinary.so
+touch "$1"/libinary.a
 echo | gcc -c -x c - -o "$1"/obj.o
 for sec in $(readelf -WS "$1"/obj.o | grep "\[.*\] \."| cut -f 2 -d "]" | tr -s " " | cut -f2 -d" ") ; do
     objcopy -R$sec "$1"/obj.o &>/dev/null || true
