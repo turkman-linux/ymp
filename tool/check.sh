19de3662e5950815e3250f0f1951d3c8bd82f8d9
aliriza <aliriza.keskin@pardus.org.tr>
2022-11-03 08:55:51 +0000

    mimedata

--- tool/check.sh
+++ tool/check.sh
@@ -4,7 +4,7 @@ touch "$1"/ymp.h
 touch "$1"/ymp-cli
 touch "$1"/libymp.so
 touch "$1"/libymp.a
-echo | gcc -c -x c - -o "$1"/obj.o
+echo -n | gcc -c -x c - -o "$1"/obj.o
 for sec in $(readelf -WS "$1"/obj.o | grep "\[.*\] \."| cut -f 2 -d "]" | tr -s " " | cut -f2 -d" ") ; do
     objcopy -R$sec "$1"/obj.o &>/dev/null || true
 done
