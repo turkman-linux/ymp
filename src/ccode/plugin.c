bfcd25611644ca12b8e5a42e1a88c817fd634923
aliriza <aliriza.keskin@pardus.org.tr>
2023-03-03 11:52:42 +0000

    fix build

--- src/ccode/plugin.c
+++ src/ccode/plugin.c
@@ -2,20 +2,17 @@
 #define _plugin
 #include <stdio.h>
 #include <dlfcn.h>
-#include <ymp-extra.h>
 void load_plugin(char* path){
-    try{
-        void *handle;
-        handle = dlopen(path, RTLD_LAZY);
-        if (!handle) {
-            fprintf(stderr, "Failed to load plugin: %s from %s\n ",dlerror(), path);
-            return;
-        }
-        dlerror();
-        void (*plugin_func)();
-        *(void**)(&plugin_func) = dlsym(handle, "plugin_init");
-        plugin_func();
-        dlclose(handle);
+    void *handle;
+    handle = dlopen(path, RTLD_LAZY);
+    if (!handle) {
+        fprintf(stderr, "Failed to load plugin: %s from %s\n ",dlerror(), path);
+        return;
     }
+    dlerror();
+    void (*plugin_func)();
+    *(void**)(&plugin_func) = dlsym(handle, "plugin_init");
+    plugin_func();
+    dlclose(handle);
 }
 #endif
