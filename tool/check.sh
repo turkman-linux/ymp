f14dc1665d2c5942bdab7d1a4726a12f1c3e7a89
aliriza.keskin <aliriza.keskin@pardus.org.tr>
2023-09-26 09:40:38 +0000

    reduce empty.o file

--- tool/check.sh
+++ tool/check.sh
@@ -8,7 +8,7 @@ touch "$1"/ymp-cli-static
 touch "$1"/ymp-shell-static
 touch "$1"/libymp.so
 touch "$1"/libymp-static.a
-echo -n | gcc -c -x c - -o "$1"/obj.o
+as -o "$1"/obj.o data/obj.s
 for sec in $(readelf -WS "$1"/obj.o | grep "\[.*\] \."| cut -f 2 -d "]" | tr -s " " | cut -f2 -d" ") ; do
     objcopy -R$sec "$1"/obj.o &>/dev/null || true
 done
