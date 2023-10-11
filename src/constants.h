fc6b11cfbd5dbf10735ac2f28748c13041538d54
aliriza <aliriza.keskin@pardus.org.tr>
2023-10-11 07:21:15 +0000

    color.vala replaced with define

---
+++ src/constants.h
@@ -0,0 +1,11 @@
+//DOC: ## Colors
+//DOC: Available colors
+//DOC: black, red, green, yellow, blue, magenta, cyan, white
+#define black 30
+#define red 31
+#define green 32
+#define yellow 33
+#define blue 34
+#define magenta 35
+#define cyan 36
+#define white 37
--- tool/preprocessor.sh
+++ tool/preprocessor.sh
@@ -2,5 +2,5 @@ output=$1
 shift
 for arg in $@ ; do
     mkdir -p $(dirname $output/$arg)
-    cat $arg | gcc -E - | sed "/^#.*/d"> $output/$arg
+    cat src/constants.h $arg | gcc -E - | sed "/^#.*/d"> $output/$arg
 done
