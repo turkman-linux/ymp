5d9fec725c2e944959f2a5392a2e229dcbb86627
aliriza <aliriza.keskin@pardus.org.tr>
2023-10-16 11:54:56 +0000

    remove libc :D

--- src/libc/start.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-
-int main(int argc, char** argv, char** envp);
-void __libc_start_main(int argc, char** argv, char** envp){
-    exit(main(argc, argv, envp));
-}
--- src/libc/x86_64/start.s
+++
@@ -1,9 +0,0 @@
-.global _start
-.section .note.GNU-stack
-.section .text
-_start:
-	popq	%rdi
-	movq	%rsp, %rsi
-	movq	8(%rsp, %rdi, 8), %rdx
-	call	__libc_start_main
-
