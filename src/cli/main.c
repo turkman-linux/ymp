9a8bcfde3fff5001f976ccd5734ecc843a21dceb
sulincix <parduscix@yandex.ru>
2023-11-03 17:58:32 +0000

    cli move to C

---
+++ src/cli/main.c
@@ -0,0 +1,53 @@
+#if no_locale
+char* _(char* msg){
+    return msg;
+}
+#else
+#include <libintl.h>
+#include <locale.h>
+#endif
+#include <ymp.h>
+#include <stdio.h>
+
+int main(int argc, char** argv){
+#ifndef no_locale
+    setlocale(LC_ALL, "");
+    bindtextdomain("ymp", NULL);
+    textdomain("ymp");
+    #define _ gettext
+#endif
+    /*
+    ██████╗  ██╗
+    ╚════██╗███║
+     █████╔╝╚██║
+     ╚═══██╗ ██║
+    ██████╔╝ ██║
+    ╚═════╝  ╚═╝
+    */
+    Ymp *ymp = ymp_init(argv, argc);
+    if(get_bool("sandbox")){
+        array *a = array_new();
+        array_adds(a, argv, argc);
+        array_remove(a, "--sandbox");
+        return run_sandbox_main(array_get(a,NULL),array_length(a));
+    }if (get_bool("version")){
+        write_version();
+        return 0;
+    }if(argc < 2){
+        gchar* c1 = g_strconcat(_ ("No command given."), "\n", NULL);
+        gchar* c2 = g_strdup_printf ("\x1b[%dm%s\x1b[;0m", 31, _ ("ymp help"));
+        gchar* c3 = g_strdup_printf (_ ("Run %s for more information about usage."), c2);
+        gchar* msg =  g_strconcat (c1, c3, NULL);
+        error_add (msg);
+        free(msg);
+        free(c3);
+        free(c2);
+        free(c1);
+        error (31);
+    }else {
+        ymp_add_process(ymp, argv[1], argv+2, argc-2);
+    }
+    ymp_run(ymp);
+    error(1);
+    return 0;
+}
