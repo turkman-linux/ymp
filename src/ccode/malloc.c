68f2386eff430866bb1f150dd38e8ea138a7c873
aliriza <aliriza.keskin@pardus.org.tr>
2023-11-08 11:25:35 +0000

    malloc replaced with calloc in libymp

---
+++ src/ccode/malloc.c
@@ -0,0 +1,10 @@
+#include <stddef.h>
+#include <stdlib.h>
+void* g_malloc(size_t size){ 
+    return (void*) calloc((size_t)1, size);
+}
+
+void* malloc(size_t size){
+    return (void*) calloc((size_t)1, size);
+}
+
